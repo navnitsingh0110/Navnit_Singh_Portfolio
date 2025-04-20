ceef8956ede3458980a325955ca8fe4f2ce41a1f
navnitsingh0110 <ns011125@gmail.com>
2025-04-20 21:20:06 +0530

    images path changed

--- src/components/Hero/HeroPic.jsx
+++ src/components/Hero/HeroPic.jsx
@@ -7,13 +7,13 @@ function HeroPic() {
   return (
     <motion.div variants={fadeIn('left',0.2)} initial='hidden' whileInView='show' viewport={{once: false, amount:0}} className="h-full flex items-center justify-center">
       <img
-        src="../../public/images/my-img.png"
+        src="/images/my-img.png"
         alt="Navnit Singh"
         className="max-h-[350px] w-auto"
       />
-      <div className="absolute -z-10 flex justify-center items-center animate-pulse">
+      {/* <div className="absolute -z-10 flex justify-center items-center animate-pulse">
         <PiSquare className="md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_3s_linear_infinite]" />
-      </div>
+      </div> */}
     </motion.div>
   );
 }
