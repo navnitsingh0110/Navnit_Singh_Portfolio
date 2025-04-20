133b5881bb64499ea4d5f28a31e11887f4016dad
navnitsingh0110 <ns011125@gmail.com>
2025-04-20 21:47:00 +0530

    changed font size

--- src/components/Navbar/NavbarLogo.jsx
+++ src/components/Navbar/NavbarLogo.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function NavbarLogo() {
   return (
     <div>
-        <h1 className='text-white text-2xl sm:hidden md:block'>Navnit Singh</h1>
-        <h1 className='text-white font-extrabold text-4xl sm:block md:hidden'>NS</h1>
+        <h1 className='text-white text-3xl sm:hidden md:block'><span className='text-orange text-4xl'>N</span>avnit <span className='text-orange text-4xl'>S</span>ingh</h1>
+        <h1 className='text-orange font-extrabold text-4xl sm:block md:hidden'>NS</h1>
     </div>
   )
 }
