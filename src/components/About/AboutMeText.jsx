9113c3ba57e5066731aaed7acd13304f70ce9dcc
navnitsingh0110 <ns011125@gmail.com>
2025-05-10 18:15:09 +0530

    Added resume to about me section

--- src/components/About/AboutMeText.jsx
+++ src/components/About/AboutMeText.jsx
@@ -7,14 +7,27 @@ function AboutMeText() {
       <p className='text-white'>
         As a passionate Front-End Developer, I specialize in creating responsive, user-friendly websites and applications. With expertise in HTML, CSS, JavaScript, React, and Java, I bring a strong understanding of front-end technologies and design principles to every project. I’m committed to writing clean, efficient code and building seamless user experiences. Whether working independently or as part of a team, I thrive in dynamic environments where innovation and collaboration are key. Looking to connect with like-minded professionals and explore new opportunities to continue growing and making a positive impact in the tech industry.
       </p>
-      <a
-        href="https://github.com/navnitsingh0110"
-        target="_blank"
-        rel="noopener noreferrer"
-        className='border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-black transition-all duration-500 cursor-pointer md:self-start sm:self-center text-center text-white hover:text-cyan'
-      >
-        My Projects
-      </a>
+
+      {/* BUTTON ROW */}
+      <div className="flex mt-10 space-x-4 justify-center md:justify-start">
+        <a
+          href="https://github.com/navnitsingh0110"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='border border-orange rounded-full py-2 px-4 text-lg hover:bg-black transition-all duration-500 cursor-pointer text-white hover:text-cyan'
+        >
+          My Projects
+        </a>
+        <a
+          href="/Navnit_Singh_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='border border-orange rounded-full py-2 px-4 text-lg hover:bg-black transition-all duration-500 cursor-pointer text-white hover:text-cyan'
+        >
+          My Resume
+        </a>
+      </div>
+
     </div>
   )
 }
